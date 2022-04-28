f5a239633d2e917ca668379f68e49f5bac4710ac
subhamtulshan <subhamtulshan123@gmail.com>
2022-04-28 13:12:41 +0530

    Update Alien Dictionary.java

--- Alien Dictionary.java
+++ Alien Dictionary.java
@@ -18,17 +18,18 @@ public class Solution
 			String w1=words[i];
 			String w2=words[i+1];
 			int minL=Math.min(w1.length(),w2.length());
+			//this check for the condition where two prefix are same but large word occur later 
 			if(w1.length()>w2.length() && w1.substring(0,minL)==w2.substring(0,minL))
 				return "";
 			for(int j=0;j<minL;j++)
 			{
 				if(w1.charAt(j)!=w2.charAt(j))
 				{
-                    adj.putIfAbsent(w1.charAt(j),new HashSet<Character>());
+                   			adj.putIfAbsent(w1.charAt(j),new HashSet<Character>());
 					HashSet<Character> d= adj.get(w1.charAt(j));
-                    d.add(w2.charAt(j));
-                    adj.remove(w1.charAt(j));
-                    adj.put(w1.charAt(j),d);
+                    			d.add(w2.charAt(j));
+                    			adj.remove(w1.charAt(j));
+                    			adj.put(w1.charAt(j),d);
 					break;
 				}
 			}
@@ -36,17 +37,20 @@ public class Solution
         for(Map.Entry m : adj.entrySet())
         {    
             if(dfs((Character)m.getKey()))
-				return "";
+		return "";
         }  	
         System.out.println(res.reverse());
         return res.toString();		
     }
+	
 	public Boolean dfs(Character c) 
 	{
-		if(visited.containsKey(c))
-			return visited.get(c);
-		
-		visited.put(c,true);
+	//if the node is in the hashMap it mean either its been visited(false) or its in the path(true)-> mean there is a cycle
+	if(visited.containsKey(c))
+		return visited.get(c);
+	//Here the true mean the current node is in the path. 
+	visited.put(c,true);
+	
         if(adj.containsKey(c))
         {
             for(Character cc :adj.get(c))
@@ -55,8 +59,10 @@ public class Solution
                     return true;
             }
         }
+	//here false mean the path is visited and not in the path
         visited.put(c,false);
-		res.append(c);
+	//the char is added after all its child are done post order dfs a->b->c and a->c // try this case with normal dfs wrong ans ayega
+	res.append(c);
         return false;
     }
 }
